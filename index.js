e9199dd4aaae392b5834390b8df74822e1581dfc
Daniel Díaz <daniel0diiaz@gmail.com>
2021-09-23 18:52:02 +0200

    Quick fixes. Also add printer function

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 const fetch = require('node-fetch'),
     fs = require('fs'),
     PDFLib = require('pdf-lib'),
-    PDFDocument = PDFLib.PDFDocument,
-    rgb = PDFLib.rgb,
-    fontkit = require('@pdf-lib/fontkit');
+    fontkit = require('@pdf-lib/fontkit'),
+    printer = require('pdf-to-printer'),
+    path = require('path');
 
 async function main(offset=0, pagination=true, split_range=[-1, -1]) {
-
     let files  = fs.readdirSync('./in/'),
-        total_pages = 0;
+        results = [];
     for(let f in files) {
-        let file = {
-            format: files[f].substr(-4),
-            name: files[f].slice(0, -4)
-        }
-        if(file.format===".pdf") {
-            let inDoc = await PDFDocument.load(fs.readFileSync('./in/'+files[f]), {ignoreEncryption: true}),
-                first = await PDFDocument.create(),
-                second = await PDFDocument.create();
+        console.log("Dividiendo archivo "+files[f]);
+        results.push(await create_doublesidepdf(files[f], offset, pagination, split_range).then(async res => {
+            console.log('Documento '+res.name+':\n- Páginas totales: '+res.pages+'\n- Páginas del documento 1: '+res.documents[0].pages+'\n- Páginas del documento 2: '+res.documents[1].pages);
 
-            let pages = inDoc.getPages();
-            split_range[0] = split_range[0]===-1 ? offset : split_range;
-            split_range[1] = split_range[1]===-1 ? pages.length : split_range;
+            console.log('Imprimiendo archivo 2. Pulsa enter cuando haya acabado y gires los folios en la impresora.');
+            printer.print(path.join(__dirname, './out/'+res.name+'/second.pdf'));
+            await enterToContinue();
+            console.log('Imprimiendo archivo 1.');
+            printer.print(path.join(__dirname, './out/'+res.name+'/first.pdf'));
+        }));
+    }
+    console.log('Trabajo terminado. '+results.length+' ficheros divididos.');
+}
 
+async function create_doublesidepdf(file, offset, pagination, split_range) {
+    let PDFDocument = PDFLib.PDFDocument,
+        fileinfo = {
+            format: file.substr(-4),
+            name: file.slice(0, -4)
+        };
+
+    if(fileinfo.format===".pdf") {
+        let inDoc = await PDFDocument.load(fs.readFileSync('./in/'+file), {ignoreEncryption: true}),
+            first = await PDFDocument.create(),
+            second = await PDFDocument.create();
+
+        let pages = inDoc.getPages();
+        let range = [
+            split_range[0]===-1 ? offset : split_range[0],
+            split_range[1]===-1 ? pages.length : split_range[1]
+        ];
+        return await new Promise(async resolve => {
+            let result = {
+                name: fileinfo.name,
+                pages: pages,
+                documents: []
+            };
             await new Promise(async res => {
                 for(let i=offset, z=0;i<pages.length;i++, z++) {
-                    if(i>=split_range[0]&&i<=split_range[1]) {
+                    if(i>=range[0]&&i<=range[1]) {
                         if(pagination) {
                             pages[i] = await add_pagination(inDoc, i, z+1);
                         }
@@ -37,33 +60,37 @@ async function main(offset=0, pagination=true, split_range=[-1, -1]) {
                             second.addPage(oddPage);
                         }
                     }
+                    progressbar(i+1, pages.length);
                 }
                 res();
             }).then(async () => {
-                if(!fs.existsSync('./out/'+file.name)) {
-                    fs.mkdirSync('./out/'+file.name);
+                if(!fs.existsSync('./out/'+fileinfo.name)) {
+                    fs.mkdirSync('./out/'+fileinfo.name);
                 }
-                fs.writeFileSync('./out/'+file.name+'/first.pdf', await first.save());
+                fs.writeFileSync('./out/'+fileinfo.name+'/first.pdf', await first.save());
                 if(pages.length>1) {
-                    fs.writeFileSync('./out/'+file.name+'/second.pdf', await second.save());
+                    fs.writeFileSync('./out/'+fileinfo.name+'/second.pdf', await second.save());
                 }
-                console.log('Páginas del documento 1: '+first.getPages().length);
-                console.log('Páginas del documento 2: '+second.getPages().length);
-                total_pages += first.getPages().length + second.getPages().length;
+                result.documents = [
+                    { pages: first.getPages().length },
+                    { pages: second.getPages().length }
+                ];
+                result.pages = first.getPages().length + second.getPages().length;
+                resolve(result);
             }).catch(e => {
                 console.log(e);
             });
-        } else {
-            console.log('Error: el formato del archivo '+files[f]+' no es PDF');
-        }
+        });
+    } else {
+        console.log('Error: el formato del archivo '+file+' no es PDF');
     }
-    console.log('Páginas totales: '+total_pages);
 }
 
 async function add_pagination(pdfDoc, pageIndex, pageNumber) {
     const fontBytes = await fetch('https://pdf-lib.js.org/assets/ubuntu/Ubuntu-R.ttf').then(res => res.arrayBuffer());
     pdfDoc.registerFontkit(fontkit);
-    const customFont = await pdfDoc.embedFont(fontBytes);
+    const customFont = await pdfDoc.embedFont(fontBytes),
+        rgb = PDFLib.rgb;
 
     let page = pdfDoc.getPages()[pageIndex];
 
@@ -77,6 +104,25 @@ async function add_pagination(pdfDoc, pageIndex, pageNumber) {
     return page;
 }
 
+function progressbar(i, limit, dots_limit=20) {
+    const percentage = i/limit,
+        dots_percentage = parseInt(percentage * dots_limit),
+        dots = ".".repeat(dots_percentage),
+        left = dots_limit - dots_percentage,
+        empty = " ".repeat(left);
+    process.stdout.write(`\r[${dots}${empty}] ${(percentage*100).toFixed(2)}%`);
+    if(i/limit===1) {
+        console.log('');
+    }
+}
+
+function enterToContinue() {
+    process.stdin.setRawMode(true)
+    return new Promise(resolve => process.stdin.once('data', () => {
+        process.stdin.setRawMode(false);
+        resolve();
+    }));
+}
 
 let offset = 0;
 if(process.argv.includes('--offset')||process.argv.includes('-o')) {
